5b9b1cde72f1b3668a51cde7314666f3ec07e992
aanno <aanno@users.noreply.github.com>
2021-05-14 18:22:29 +0200

    remove sudo from docker helper scripts; always use podman (instead of docker)

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 for dir in */ ; do
     cd "$dir"
-    sudo podman build --rm -t "pacur/${dir::-1}" .
+    podman build --rm -t "pacur/${dir::-1}" .
     cd ..
 done

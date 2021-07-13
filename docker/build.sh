82c0e2b2f6fe0d86c1e88d8ccd93a67dbe72acc1
aanno <aanno@users.noreply.github.com>
2021-07-13 19:38:37 +0200

    download go only once, improve f34 Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -2,8 +2,15 @@
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+if [ ! -f go1.16.5.linux-amd64.tar.gz ]; then
+    wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
+    echo "b12c23023b68de22f74c0524f10b753e7b08b1504cb7e417eccebdd3fae49061 go1.16.5.linux-amd64.tar.gz" \
+    | sha256sum -c -
+fi
+
 #for dir in */ ; do
 for dir in fedora-34/ ; do
+    cp go1.16.5.linux-amd64.tar.gz "$dir"
     cd "$dir"
     podman build --rm -t "pacur/${dir::-1}" .
     cd ..
--- docker/update.sh
+++ docker/update.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+if [ ! -f go1.16.5.linux-amd64.tar.gz ]; then
+    wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
+    echo "b12c23023b68de22f74c0524f10b753e7b08b1504cb7e417eccebdd3fae49061 go1.16.5.linux-amd64.tar.gz" \
+    | sha256sum -c -
+fi
+
 # for dir in */ ; do
 for dir in fedora-34/ ; do
+    cp go1.16.5.linux-amd64.tar.gz "$dir"
     cd "$dir"
     sed -i -e "s|go get github.com/aanno/pacur@aanno-develop.*|go get github.com/aanno/pacur@aanno-develop # $(date)|g" Dockerfile
     podman build --rm -t "pacur/${dir::-1}" .

73d4ba0e25db7b767d4226a735578f22b58e12ff
aanno <aanno@users.noreply.github.com>
2021-07-13 19:15:13 +0200

    docker build and update only for fedora-34

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,8 @@
 set -e
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-for dir in */ ; do
+#for dir in */ ; do
+for dir in fedora-34/ ; do
     cd "$dir"
     podman build --rm -t "pacur/${dir::-1}" .
     cd ..
--- docker/update.sh
+++ docker/update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-for dir in */ ; do
+# for dir in */ ; do
+for dir in fedora-34/ ; do
     cd "$dir"
     sed -i -e "s|go get github.com/aanno/pacur@aanno-develop.*|go get github.com/aanno/pacur@aanno-develop # $(date)|g" Dockerfile
     podman build --rm -t "pacur/${dir::-1}" .
